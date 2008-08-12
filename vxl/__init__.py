c7b6f94a92a86e3788680486f645307c137389d8
Erik Svensson <erik@coldstar.net>
2008-08-12 23:56:18 +0200

    list fetch field hashString in torrent-get

--- vxl/__init__.py
+++ vxl/__init__.py
@@ -1,2 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 2008-08, Erik Svensson <erik.public@gmail.com>
+
 from constants import *
 from transmission import Torrent, Session, Transmission, TransmissionError

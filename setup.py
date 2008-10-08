d50484e1c93df37a7d1f9107fc48585ff4f9b2cd
Erik Svensson <erik@coldstar.net>
2008-10-08 21:02:45 +0200

    Remove vxl from transmission module.
    --HG--
    rename : transmission/vxl.py => vxl.py

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@
 
 from setuptools import setup
 
-setup(name='transmission',
-      version='0.1',
-      description='Transmission control module including cli.',
-      author='Erik Svensson',
-      author_email='erik.public@gmail.com',
-      url='http://coldstar.net/',
-      license='MIT',
-      keywords='transmission',
-      packages=['transmission'],
-      install_requires = ['simplejson>=1.7.1'],
-      entry_points = {'console_scripts': ['vxl = transmission.vxl:main']}
-      )
+setup(
+    name='transmission',
+    version='0.1',
+    description='Transmission control module.',
+    author='Erik Svensson',
+    author_email='erik.public@gmail.com',
+    url='http://coldstar.net/',
+    license='MIT',
+    keywords='transmission',
+    packages=['transmission'],
+    install_requires = ['simplejson>=1.7.1'],
+    zip_safe=True
+    )

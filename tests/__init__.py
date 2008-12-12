858e64bbc98c896248b4495aa12975f68f75e7b3
Erik Svensson <erik.svensson@shapeline.se>
2008-12-12 09:42:48 +0100

    Added tests for transmission.utils. transmission.utils.format_size now handles 1024 correctly.

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,9 +2,10 @@
 # 2008-12, Erik Svensson <erik.public@gmail.com>
 
 import unittest
-import torrent
+import torrent, utils
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(torrent.suite())
+    suite.addTest(utils.suite())
     return suite

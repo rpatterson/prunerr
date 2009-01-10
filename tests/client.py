d8270582c41b8f916f980c4d16818e360e69c7e6
Erik Svensson <erik@coldstar.net>
2009-01-11 00:28:48 +0100

    Moved http handling to a new function for simplified testing. Added more tests.

--- tests/client.py
+++ tests/client.py
@@ -1,29 +1,40 @@
 # -*- coding: utf-8 -*-
 # 2008-12, Erik Svensson <erik.public@gmail.com>
 
+import sys, os.path, base64
 import unittest
+
+if sys.version_info[0] >= 2 and sys.version_info[1] >= 6:
+    import json
+else:
+    import simplejson as json
+
 import transmission.constants
-from transmission import Client
+from transmission import TransmissionError, Client
 
 class client(unittest.TestCase):
     def assertTransmissionRequest(self, expected, client, call, *args, **kwargs):
-        
         def request(method, arguments={}, ids=[], require_ids=False):
-            if method != expected['method']:
-                raise ValueError()
-            if arguments != expected['arguments']:
-                raise ValueError()
-            if ids != expected['ids']:
-                raise ValueError()
-            if require_ids != expected['require_ids']:
-                raise ValueError()
+            self.assertEqual(method, expected['method'])
+            self.assertEqual(arguments, expected['arguments'])
+            self.assertEqual(ids, expected['ids'])
+            self.assertEqual(require_ids, expected['require_ids'])
         original_request = client._request
         client._request = request
-        try:
-            call(*args, **kwargs)
-        except Exception, e:
-            self.fail(e)
+        call(*args, **kwargs)
         client._request = original_request
+    
+    def assertTransmissionQuery(self, expected, result, client, call, *args, **kwargs):
+        def query(q):
+            data = json.loads(q)
+            self.assertEqual(data['method'], expected['method'])
+            self.assertEqual(data['arguments'], expected['arguments'])
+            result['result'] = 'success'
+            return json.dumps(result)
+        original_method = client._http_query
+        client._http_query = query
+        call(*args, **kwargs)
+        client._http_query = original_method
     
     def testConstruction(self):
         tc = Client()
@@ -58,11 +69,69 @@ class client(unittest.TestCase):
         self.assertTransmissionRequest(expected, tc, tc.add, data, peer_limit=10)
         expected = {'method': 'torrent-add', 'arguments': {'metainfo': data, 'paused': 1, 'download-dir': '/tmp', 'peer-limit': 10}, 'ids': [], 'require_ids': False}
         self.assertTransmissionRequest(expected, tc, tc.add, data, paused=True, download_dir='/tmp', peer_limit=10)
-        
+        # test exception for non integer peer_limit
         self.failUnlessRaises(ValueError, tc.add, data, peer_limit='apa')
     
     def testAddUrl(self):
-        pass
+        dirpath = os.path.dirname(os.path.abspath(__file__))
+        tc = Client()
+        data = base64.b64encode('torrent')
+        expected = {'method': 'torrent-add', 'arguments': {'metainfo': data, 'paused': 1, 'download-dir': '/tmp', 'peer-limit': 10}, 'ids': [], 'require_ids': False}
+        self.assertTransmissionRequest(expected, tc, tc.add_url, '%s/torrent.txt' % (dirpath), paused=True, download_dir='/tmp', peer_limit=10)
+        self.failUnlessRaises(TransmissionError, tc.add_url, 'torrent.torrent')
+        # TODO: Add test for real web url's?
+    
+    def testRemove(self):
+        tc = Client()
+        ids = ['0123456789abcdef', 2, 3]
+        # test at query interface
+        expected = {'method': 'torrent-remove', 'arguments': {}, 'ids': ids, 'require_ids': True}
+        self.assertTransmissionRequest(expected, tc, tc.remove, ids)
+        # test at http interface
+        expected = {'method': 'torrent-remove', 'arguments': {'ids': ids}}
+        self.assertTransmissionQuery(expected, {}, tc, tc.remove, ids)
+    
+    def testStart(self):
+        tc = Client()
+        ids = ['0123456789abcdef', 2, 3]
+        # test at query interface
+        expected = {'method': 'torrent-start', 'arguments': {}, 'ids': ids, 'require_ids': True}
+        self.assertTransmissionRequest(expected, tc, tc.start, ids)
+        # test at http interface
+        expected = {'method': 'torrent-start', 'arguments': {'ids': ids}}
+        self.assertTransmissionQuery(expected, {}, tc, tc.start, ids)
+    
+    def testStop(self):
+        tc = Client()
+        ids = ['0123456789abcdef', 2, 3]
+        # test at query interface
+        expected = {'method': 'torrent-stop', 'arguments': {}, 'ids': ids, 'require_ids': True}
+        self.assertTransmissionRequest(expected, tc, tc.stop, ids)
+        # test at http interface
+        expected = {'method': 'torrent-stop', 'arguments': {'ids': ids}}
+        self.assertTransmissionQuery(expected, {}, tc, tc.stop, ids)
+    
+    def testStop(self):
+        tc = Client()
+        ids = ['0123456789abcdef', 2, 3]
+        # test at query interface
+        expected = {'method': 'torrent-verify', 'arguments': {}, 'ids': ids, 'require_ids': True}
+        self.assertTransmissionRequest(expected, tc, tc.verify, ids)
+        # test at http interface
+        expected = {'method': 'torrent-verify', 'arguments': {'ids': ids}}
+        self.assertTransmissionQuery(expected, {}, tc, tc.verify, ids)
+    
+    def testInfo(self):
+        tc = Client()
+        fields = transmission.constants.FIELDS
+        ids = [2, 3]
+        # test at query interface
+        expected = {'method': 'torrent-get', 'arguments': {'fields': fields}, 'ids': ids, 'require_ids': True}
+        self.assertTransmissionRequest(expected, tc, tc.info, ids)
+        # test at http interface
+        result = {}
+        expected = {'method': 'torrent-get', 'arguments': {'ids': ids, 'fields': fields}}
+        self.assertTransmissionQuery(expected, {}, tc, tc.info, ids)
 
 def suite():
     suite = unittest.TestLoader().loadTestsFromTestCase(client)

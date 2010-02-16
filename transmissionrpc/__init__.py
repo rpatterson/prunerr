2e916c724df076e5723b6c907e73e509bf8c6c7f
Erik Svensson <erik.public@gmail.com>
2010-02-16 01:32:13 +0100

    version++

--- transmissionrpc/__init__.py
+++ transmissionrpc/__init__.py
@@ -6,6 +6,6 @@ from constants import *
 from transmission import TransmissionError, Torrent, Session, Client
 
 __author__    = u'Erik Svensson <erik.public@gmail.com>'
-__version__   = u'0.4'
+__version__   = u'0.5'
 __copyright__ = u'Copyright (c) 2008-2010 Erik Svensson'
 __license__   = u'MIT'

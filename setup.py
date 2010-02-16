44febb7c08c37c39fc17ab1b3905d3860f7278ff
Erik Svensson <erik.public@gmail.com>
2010-02-16 01:32:13 +0100

    version++

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='transmissionrpc',
-    version='0.4',
+    version='0.5',
     description='Python module that implements the Transmission bittorent RPC protocol.',
     author='Erik Svensson',
     author_email='erik.public@gmail.com',

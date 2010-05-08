2f508fdf284f7d73d661994712d049aa041bb6ba
Erik Svensson <erik.public@gmail.com>
2010-05-08 21:33:22 +0200

    Remove old scripts and tools.

--- contrib/assassinate.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os, signal, system
-
-# kill all instances of transmission-daemon
-for process in system.process_list():
-    if process[1][-19:] == 'transmission-daemon':
-        print('Kill %d: %s' % (process[0], process[1]))
-        os.kill(process[0], signal.SIGKILL)

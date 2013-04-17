d868f3060cd33c334a0fb714b2c65a76616bed23
Erik Svensson <erik.public@gmail.com>
2013-04-17 19:23:29 +0200

    Bump version.

--- transmissionrpc/__init__.py
+++ transmissionrpc/__init__.py
@@ -12,7 +12,7 @@ from transmissionrpc.utils import add_stdout_logger, add_file_logger
 
 __author__    		= 'Erik Svensson <erik.public@gmail.com>'
 __version_major__   = 0
-__version_minor__   = 10
+__version_minor__   = 11
 __version__   		= '{0}.{1}'.format(__version_major__, __version_minor__)
 __copyright__ 		= 'Copyright (c) 2008-2013 Erik Svensson'
 __license__   		= 'MIT'

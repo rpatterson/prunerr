c26a4600c30ed12206dd0f3967cbadb2943b1e11
Ross Patterson <me@rpatterson.net>
2020-03-29 21:19:02 -0700

    feat(cli): Add CLI options test coverage

--- src/pythonprojectstructure/tests/test_pythonprojectstructure.py
+++ src/pythonprojectstructure/tests/test_pythonprojectstructure.py
@@ -14,6 +14,16 @@ class PythonProjectStructureTests(unittest.TestCase):
     python-project-structure unit and integration tests.
     """
 
+    def getCliErrorMessages(self, args):
+        """
+        Run the CLI script and return any error messages.
+        """
+        stderr_file = io.StringIO()
+        with self.assertRaises(SystemExit):
+            with contextlib.redirect_stderr(stderr_file):
+                pythonprojectstructure.main(args=args)
+        return stderr_file.getvalue()
+
     def test_cli_help(self):
         """
         The command line script is self-docummenting.
@@ -27,4 +37,24 @@ class PythonProjectStructureTests(unittest.TestCase):
             pythonprojectstructure.__doc__.strip(),
             stdout,
             "The console script name missing from --help output",
+        )
+
+    def test_cli_options(self):
+        """
+        The command line script accepts options controlling behavior.
+        """
+        result = pythonprojectstructure.main(args=[])
+        self.assertIsNone(
+            result, "Wrong console script options return value",
+        )
+
+    def test_cli_option_errors(self):
+        """
+        The command line script displays useful messages for invalid option values.
+        """
+        stderr = self.getCliErrorMessages(args=["--non-existent-option"])
+        self.assertIn(
+            "error: unrecognized arguments: --non-existent-option",
+            stderr,
+            "Wrong invalid option message",
         )

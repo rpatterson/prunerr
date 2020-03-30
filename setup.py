36f4d5ec4dcd84551b4ae40b64dad89dcd8d9658
Ross Patterson <me@rpatterson.net>
2020-03-30 00:13:24 -0700

    build(python): Cleanup remnant from change to version from VCS tags

--- setup.py
+++ setup.py
@@ -2,20 +2,13 @@
 Python project structure foundation or template, distribution/package metadata.
 """
 
-import sys
-import os
-
 import setuptools
-
-sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
-import pythonprojectstructure  # noqa
 
 with open("README.rst", "r") as readme:
     LONG_DESCRIPTION = readme.read()
 
 setuptools.setup(
     name="python-project-structure",
-    version=pythonprojectstructure.__version__,
     author="Ross Patterson",
     author_email="me@rpatterson.net",
     description="Python project structure foundation or template",

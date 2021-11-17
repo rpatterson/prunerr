ac2fa54edba8434de393d8dc7be8784f054c8508
Ross Patterson <me@rpatterson.net>
2021-11-17 07:42:52 -0800

    style(lint): Address Pylint errors and warnings
    We don't use Pylint so far either in the local checks nor in CI/CD.  My editor uses it
    automatically and I know I find it's warnings more useful than annoying, mostly to
    catch me when I'm straying from best practices.  I know others find Pylint to be too
    strict, annoying and to get in their way.  Thus I'm on the fence about adding it too
    this template, but maybe I can figure a way to make it optional.

--- src/pythonprojectstructure/__init__.py
+++ src/pythonprojectstructure/__init__.py
@@ -3,9 +3,10 @@ Python project structure foundation or template, top-level package.
 """
 
 # Manage version through the VCS CI/CD process
+__version__ = None
 try:
     from . import version
 except ImportError:  # pragma: no cover
-    version = None
-if version is not None:  # pragma: no cover
+    pass
+else:  # pragma: no cover
     __version__ = version.version

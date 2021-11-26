81fb84bc272fb0f971d0d402463a28450bb35f71
Ross Patterson <me@rpatterson.net>
2021-11-26 08:53:36 -0800

    feat(log): Basic CLI logging config

--- src/pythonprojectstructure/__init__.py
+++ src/pythonprojectstructure/__init__.py
@@ -2,7 +2,11 @@
 Python project structure foundation or template, top-level package.
 """
 
+import os
+import logging
 import argparse
+
+logger = logging.getLogger(__name__)
 
 # Manage version through the VCS CI/CD process
 __version__ = None
@@ -20,8 +24,41 @@ parser = argparse.ArgumentParser(
 )
 
 
+def config_cli_logging(
+    root_level=logging.INFO, **kwargs
+):  # pylint: disable=unused-argument
+    """
+    Configure logging CLI usage first, but also appropriate for writing to log files.
+    """
+    # Want just our logger's level, not others', to be controlled by options/environment
+    logging.basicConfig(level=root_level)
+    if "DEBUG" in os.environ and os.getenv("DEBUG").strip().lower() in {
+        "1",
+        "true",
+        "yes",
+        "on",
+    }:  # pragma: no cover
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logger.setLevel(level)
+    return level
+
+
 def main(args=None):  # pylint: disable=missing-function-docstring
-    parser.parse_args(args=args)
+    # Parse CLI options and positional arguments
+    parsed_args = parser.parse_args(args=args)
+    # Avoid noisy boilerplate, functions meant to handle CLI usage should accept kwargs
+    # that match the defined option and argument names.
+    cli_kwargs = dict(vars(parsed_args))
+    # Remove any meta options and arguments, those used to direct option and argument
+    # handling, that shouldn't be passed onto functions meant to handle CLI usage.  More
+    # generally, err on the side of options and arguments being kwargs, remove the
+    # exceptions.
+    # del shared_kwargs["foo"]
+
+    # Configure logging for CLI usage
+    config_cli_logging(**cli_kwargs)
 
 
 main.__doc__ = __doc__

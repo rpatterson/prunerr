c0a041a796714b57f117390b816a995734370921
Ross Patterson <me@rpatterson.net>
2022-11-06 04:51:15 -0800

    fix(cli): Script/main exit status/code handling

--- src/pythonprojectstructure/__main__.py
+++ src/pythonprojectstructure/__main__.py
@@ -2,8 +2,10 @@
 Python project structure foundation or template, command-line execution.
 """
 
+import sys  # pragma: no cover
+
 from . import main  # pragma: no cover
 
 
 if __name__ == "__main__":  # pragma: no cover
-    main()
+    sys.exit(main())

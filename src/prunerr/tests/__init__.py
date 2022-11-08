1421caeed6b60b0464650aebf9eb81f56276f51c
Ross Patterson <me@rpatterson.net>
2022-11-08 14:45:50 -0800

    test(exec): Transmission RPC fixture
    Expands the request mocking to multiple responses per-URL for the transmission RPC API.

--- src/prunerr/tests/__init__.py
+++ src/prunerr/tests/__init__.py
@@ -2,11 +2,12 @@
 Tests for Prunerr.
 """
 
-import os
+import re
 import pathlib
 import mimetypes
 import email.utils
 import urllib.parse
+import json
 
 import unittest
 
@@ -25,6 +26,9 @@ class PrunerrTestCase(unittest.TestCase):
     HOME = pathlib.Path(prunerr.__path__[0]).parents[1] / "home"
     CONFIG = HOME / ".config" / "prunerr.yml"
 
+    # From /usr/lib/python3.10/wsgiref/validate.py:340
+    HTTP_METHODS = ("GET", "HEAD", "POST", "OPTIONS", "PATCH", "PUT", "DELETE", "TRACE")
+    HTTP_METHODS_RE = re.compile(f"^({'|'.join(HTTP_METHODS)})")
     RESPONSES_DIR = pathlib.Path(__file__).parent / "responses" / "default"
 
     def setUp(self):
@@ -39,7 +43,10 @@ class PrunerrTestCase(unittest.TestCase):
         self.addCleanup(self.requests_mock.stop)
         self.requests_mock.start()
 
-    def mock_responses(self, responses_dir=RESPONSES_DIR):
+    def mock_responses(
+        self,
+        responses_dir=RESPONSES_DIR,
+    ):  # pylint: disable=too-many-locals
         """
         Mock response responses from files in the given directory.
 
@@ -50,41 +57,62 @@ class PrunerrTestCase(unittest.TestCase):
         - Potential to be re-used outside the test suite programming language
         """
         response_mocks = {}
-        for dirpath, _, filenames in os.walk(responses_dir, followlinks=True):
-            for filename in filenames:
-                mock_file_path = pathlib.Path(dirpath) / filename
-                mock_parent_relative = mock_file_path.parent.relative_to(responses_dir)
-                mock_stat = mock_file_path.stat()
-                mock_bytes = mock_file_path.read_bytes()
-                mock_headers = {
-                    "Last-Modified": email.utils.formatdate(
-                        timeval=mock_stat.st_mtime,
+        for request_headers_path in responses_dir.glob("**/request-headers.json"):
+            method = self.HTTP_METHODS_RE.match(request_headers_path.parent.name).group(
+                1
+            )
+            url_unquoted_path = pathlib.PurePosixPath(
+                urllib.parse.unquote(
+                    str(
+                        request_headers_path.parents[1].relative_to(responses_dir),
+                    ),
+                ),
+            )
+            url_path_split = urllib.parse.urlsplit(
+                str(url_unquoted_path.relative_to(url_unquoted_path.parents[-3])),
+            )
+            url_split = urllib.parse.SplitResult(
+                scheme=url_unquoted_path.parents[-2].name,  # pylint: disable=no-member
+                netloc=url_unquoted_path.parents[-3].name,  # pylint: disable=no-member
+                path=url_path_split.path,
+                query=url_path_split.query,
+                fragment=url_path_split.fragment,
+            )
+
+            responses = []
+            for response_path in request_headers_path.parent.glob("*/response.*"):
+                if response_path.name.endswith("~"):
+                    # Ignore backup files
+                    continue
+                response_headers = {}
+                response_headers_path = response_path.parent / "response-headers.json"
+                if response_headers_path.exists():
+                    response_headers = json.load(response_headers_path)
+                response_stat = response_path.stat()
+                response_bytes = response_path.read_bytes()
+
+                if "Last-Modified" not in response_headers:
+                    response_headers["Last-Modified"] = email.utils.formatdate(
+                        timeval=response_stat.st_mtime,
                         usegmt=True,
-                    ),
-                    "Content-Length": str(len(mock_bytes)),
-                }
-                mock_type, _ = mimetypes.guess_type(mock_file_path.name)
-                if mock_type:
-                    mock_headers["Content-Type"] = mock_type
-                mock_basename_split = urllib.parse.urlsplit(
-                    urllib.parse.unquote(mock_file_path.stem),
+                    )
+                if "Content-Length" not in response_headers:
+                    response_headers["Content-Length"] = str(len(response_bytes))
+                if "Content-Type" not in response_headers:
+                    response_type, _ = mimetypes.guess_type(response_path.name)
+                    if response_type:
+                        response_headers["Content-Type"] = response_type
+
+                responses.append(
+                    dict(headers=response_headers, content=response_bytes),
                 )
-                mock_url_split = urllib.parse.SplitResult(
-                    scheme=mock_parent_relative.parents[-2].name,
-                    netloc=mock_parent_relative.parents[-3].name,
-                    path=str(
-                        mock_file_path.parent.relative_to(
-                            responses_dir / mock_parent_relative.parents[-3]
-                        )
-                        / mock_basename_split.path
-                    ),
-                    query=mock_basename_split.query,
-                    fragment=mock_basename_split.fragment,
-                )
-                response_mocks[mock_url_split] = self.requests_mock.get(
-                    mock_url_split.geturl(),
-                    # Ensure the download response includes `Last-Modified`
-                    headers=mock_headers,
-                    content=mock_bytes,
-                )
+
+            with request_headers_path.open() as request_headers_opened:
+                request_headers = json.load(request_headers_opened)
+            response_mocks[url_split] = self.requests_mock.register_uri(
+                method=method,
+                url=url_split.geturl(),
+                request_headers=request_headers,
+                response_list=responses,
+            )
         return response_mocks

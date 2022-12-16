962bd47a4343cd42df9a18b34582e282601f5080
Ross Patterson <me@rpatterson.net>
2022-12-16 14:01:50 -0800

    build(debug): Post-mortem exception support
    I find I use this pattern in a lot of my code, so into the template it goes.

--- src/pythonprojectstructure/utils.py
+++ src/pythonprojectstructure/utils.py
@@ -8,3 +8,7 @@ import os
 
 TRUE_STRS = {"1", "true", "yes", "on"}
 DEBUG = "DEBUG" in os.environ and os.environ["DEBUG"].strip().lower() in TRUE_STRS
+POST_MORTEM = (
+    "POST_MORTEM" in os.environ
+    and os.environ["POST_MORTEM"].strip().lower() in TRUE_STRS
+)

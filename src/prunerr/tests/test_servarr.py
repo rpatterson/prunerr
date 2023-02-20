8bdaa4b0f593708c90789eab09d15149e350d653
Ross Patterson <me@rpatterson.net>
2023-02-20 13:14:06 -0800

    style(lint): Ignore PyLint error on single test

--- src/prunerr/tests/test_servarr.py
+++ src/prunerr/tests/test_servarr.py
@@ -12,7 +12,9 @@ from .. import tests
 
 
 @mock.patch.dict(os.environ, tests.PrunerrTestCase.ENV)
-class PrunerrServarrTests(tests.PrunerrTestCase):
+class PrunerrServarrTests(
+    tests.PrunerrTestCase,
+):  # pylint: disable=too-few-public-methods
     """
     Test Prunerr's interaction with Servarr instances.
     """

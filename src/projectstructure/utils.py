d078226d8c9b40134423f00453afc645cc5bcc35
Ross Patterson <me@rpatterson.net>
2023-05-23 20:20:26 -0700

    style(py): Enable all PyLint extensions or plugins
    It turns out that [the `enable-all-extensions` option doesn't work in configuration
    files](https://github.com/pylint-dev/pylint/issues/8217#issue-1574832612) even though
    it's documented in the example config.

--- src/projectstructure/utils.py
+++ src/projectstructure/utils.py
@@ -1,5 +1,4 @@
 # SPDX-FileCopyrightText: 2023 Ross Patterson <me@rpatterson.net>
-#
 # SPDX-License-Identifier: MIT
 
 """
@@ -12,9 +11,10 @@ import os
 
 TRUE_STRS = {"1", "true", "yes", "on"}
 DEBUG = (  # noqa: F841
-    "DEBUG" in os.environ and os.environ["DEBUG"].strip().lower() in TRUE_STRS
+    "DEBUG" in os.environ  # pylint: disable=magic-value-comparison
+    and os.environ["DEBUG"].strip().lower() in TRUE_STRS
 )
 POST_MORTEM = (  # noqa: F841
-    "POST_MORTEM" in os.environ
+    "POST_MORTEM" in os.environ  # pylint: disable=magic-value-comparison
     and os.environ["POST_MORTEM"].strip().lower() in TRUE_STRS
 )
